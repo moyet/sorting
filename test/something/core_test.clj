a71afbc84b08a52826eb08e63abf568709ef2ccc
Magnus Dreyer <magnus.dreyer@ufst.dk>
2022-05-27 17:10:42 +0200

    More test

--- test/something/core_test.clj
+++ test/something/core_test.clj
@@ -9,12 +9,12 @@
 (deftest sorting-test
   (let [a-list (gen/sample gen/small-integer)]
     (testing "Quicksort and mergesort is the same"
-      (is (= (flatten (qsort a-list)) (mergesort a-list)))
-      )
+      (is (= (flatten (qsort a-list)) (mergesort a-list))))
     (testing "Bubblesort and mergesort is the same"
-      (is (= (bubblesort a-list) (mergesort a-list)))
-      )
-    ))
+      (is (= (bubblesort a-list) (mergesort a-list))))
+    (testing "Bubblesort works on a known problem"
+      (let [a-list '(65 324 432 123 44 -12 321 342 54 12 34 23231 3) ]
+        (is (= (bubblesort a-list) '(-12 3 12 34 44 54 65 123 321 324 342 432 23231) ))))))
 
 (def property
   (prop/for-all [v (gen/vector gen/small-integer)]
